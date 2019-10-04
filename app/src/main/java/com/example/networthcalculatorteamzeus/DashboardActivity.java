f7a1bac1f30ee8ae245c0468cb2f4c07520a62a1
uchesolomon <solomon7373@yahoo.com>
2019-10-04 06:25:17 +0100

    Set the screen orientation to portrait on the main activity, sign up activity and dashboard activity
    Set the screen orientation to portrait in the manifest file on the main activity, sign up activity and dashboard activity

---
+++ app/src/main/java/com/example/networthcalculatorteamzeus/DashboardActivity.java
@@ -0,0 +1,79 @@
+package com.example.networthcalculatorteamzeus;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+        SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+        String display = preferences.getString("display","");
+
+        TextView displayInfo = (TextView) findViewById(R.id.dashText);
+        displayInfo.setText(display);
+
+        final EditText assets = (EditText) findViewById(R.id.assetValue);
+        final EditText cash = (EditText) findViewById(R.id.cashValue);
+        final EditText liabilities = (EditText) findViewById(R.id.liabilityValue);
+        final TextView netWorth = (TextView) findViewById(R.id.networthVal);
+        Button btnCalculate = (Button) findViewById(R.id.btnNetWorth);
+        Button btnLogout = (Button) findViewById(R.id.btnSignOut);
+        Button tipsBtn = (Button) findViewById(R.id.btnTips);
+        int ans = 0;
+
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (assets.getText().toString().length()==0){
+                    assets.setError("This field is required");
+                }
+                if (cash.getText().toString().length()==0){
+                    cash.setError("This field is required");
+                }
+                if (liabilities.getText().toString().length()==0){
+                    liabilities.setError("This field is required");
+                }
+                else {
+                    double assetVal = Double.parseDouble(assets.getText().toString());
+                    double cashVal = Double.parseDouble(cash.getText().toString());
+                    double liabilityVal = Double.parseDouble(liabilities.getText().toString());
+
+
+                    double result = assetVal + cashVal - liabilityVal;
+                    netWorth.setText(String.format(Locale.getDefault(), "%f", result));
+                }
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logout = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(logout);
+            }
+        });
+
+        tipsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent show = new Intent(DashboardActivity.this, Tips.class);
+                startActivity(show);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/networthcalculatorteamzeus/SignUp.java
@@ -0,0 +1,92 @@
+package com.example.networthcalculatorteamzeus;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        final EditText firstName = (EditText) findViewById(R.id.regFirstName);
+        final EditText lastName = (EditText) findViewById(R.id.regLastName);
+        final EditText email = (EditText) findViewById(R.id.regEmail);
+        final EditText phone = (EditText) findViewById(R.id.regPhone);
+        final EditText password = (EditText) findViewById(R.id.regPassword);
+        final TextView signin = (TextView) findViewById(R.id.signIn);
+        Button btnRegister = (Button) findViewById(R.id.btnReg);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailInput = email.getEditableText().toString().trim();
+                if (firstName.getText().toString().length()<2) {
+                    firstName.setError("First name should be at least 2 characters");
+                }
+                if (lastName.getText().toString().length()<2) {
+                    lastName.setError("Last name should be at least 2 characters");
+                }
+                if (email.getText().toString().length()==0) {
+                    email.setError("Field can't be empty");
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+                    email.setError("Please enter a valid email address");
+                }
+                if (phone.getText().toString().length()<8) {
+                    phone.setError("Phone number should be at least 8 characters");
+                }
+                if (password.getText().toString().length()<6) {
+                    password.setError("Password should be at least 6 characters");
+                }
+
+
+
+                SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+                String newFirstName = firstName.getText().toString();
+                String newLastName = lastName.getText().toString();
+                String newEmail = email.getText().toString();
+                String newPhone = phone.getText().toString();
+                String newPassword = password.getText().toString();
+
+                    SharedPreferences.Editor editor = preferences.edit();
+
+                    editor.putString("EMAIL", newEmail);
+                    editor.commit();
+                    editor.putString("PASSWORD", newPassword);
+                    editor.commit();
+                    editor.putString(newEmail + newPassword + "data", newFirstName );
+                    editor.commit();
+
+                    if (password.getText().toString().length()>=6 && phone.getText().toString().length()>=8
+                    && Patterns.EMAIL_ADDRESS.matcher(emailInput).matches() && lastName.getText().toString().length()>=2
+                    && firstName.getText().toString().length()>=2) {
+
+                        Intent loginScreen = new Intent(SignUp.this, MainActivity.class);
+                        startActivity(loginScreen);
+                    }
+                    else {
+                        Toast.makeText(SignUp.this,"Please fill required fields",Toast.LENGTH_LONG).show();
+                    }
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginScreen = new Intent(SignUp.this, MainActivity.class);
+                startActivity(loginScreen);
+            }
+        });
+    }
+}
